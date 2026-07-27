15bc54ef6b1c8ee612f8ebea5f2bdb7c3ea9e0f3
ayeshakiranhafeez <ayeshakiranhafeez@gmail.com>
2026-07-27 09:13:10 -0700

    finish arrays

---
+++ apps.js
@@ -0,0 +1,132 @@
+ 
+/*
+Exercise 2: Add strings to the array
+
+1) Add 'pizza' and 'cheeseburger' to the `foods` array. 
+
+Note: 'pizza' should be the first item in the array, followed by 'cheeseburger'.
+
+Complete Exercise 2 in the space below:
+*/
+
+
+foods.push()
+
+console.log('Exercise 2 result:', foods);  
+
+const foods = [];
+
+foods.unshift["pizza"];
+
+foods.push["cheseburger"];
+
+
+ 
+/*
+Exercise 6: Replace elements
+
+1) Replace 'pizza' in the `foods` array with 'sushi' and 'cupcake'.
+
+
+
+Complete Exercise 6 in the space below:
+*/
+
+let foods = ["pizza", "sushi", "cupcakes"]
+
+foods[0] = "sushi";
+
+foods.splice(0,1, "sushi, cupcakes")
+
+["sushi", "cupcakes", "burger", "fries"]
+
+console.log('Exercise 6 result:', foods);
+
+
+/*
+Exercise 1: Define an empty array
+
+1) Create an empty array and assign it to a variable called `foods`.
+
+Exercise 1 has been completed for you:
+*/
+
+const foods = [];  
+
+console.log('Exercise 1 result:', foods);
+
+
+/*
+Exercise 3: Insert at the beginning
+
+1) Insert the string 'taco' at the beginning of the `foods` array.
+
+
+const foods = ["pizza", "cheese burger",];
+
+foods.unshift("taco");
+
+
+Complete Exercise 3 in the space below:
+*/
+
+
+
+console.log('Exercise 3 result:', foods);
+
+
+/*
+Exercise 4: Access an array element
+
+1) Retrieve the 'pizza' string from the array based on its position (index) in
+   the array.  
+
+2) Assign it to a variable called `favFood`.
+
+Complete Exercise 4 in the space below:
+*/
+
+const favouritefood = foods["pizza", "cheseburger"];
+
+const favouritefood = foods[0];
+
+const favouritefood = foods[0];
+
+foods [0] 
+
+
+
+console.log('Exercise 4 result:', favFood);
+
+
+/*
+Exercise 5: Insert an element between two others
+
+1) Insert the string 'tofu' between 'pizza' and 'cheeseburger' in the array.
+
+Complete Exercise 5 in the space below:
+*/
+
+foods.splice [1, 0, "tofu"]
+
+["pizza", "tofu", "cheseburger"]
+
+
+
+console.log('Exercise 5 result:', foods);
+
+/*
+Exercise 7: Using the `slice()` method
+
+1) Use the `slice()` method to create a new array that contains 'sushi' and 
+   'cupcake'.
+
+2) Assign it to a variable named `yummy`.
+
+Complete Exercise 7 in the space below:
+*/
+
+
+
+console.log('Exercise 7 result:', yummy);
+
